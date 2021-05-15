541916ee1e0025572ebca9f4ec77cf2a74de2e70
hassanalawie <hassan.alawie10@gmail.com>
2021-05-14 23:58:16 -0400

    new picture

--- js/main.js
+++ js/main.js
@@ -312,7 +312,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 300 - Math.random() * 100;
+  var delta = 150 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
@@ -322,7 +322,7 @@ TxtRotate.prototype.tick = function() {
   } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 500;
+    delta = 50;
   }
 
   setTimeout(function() {
